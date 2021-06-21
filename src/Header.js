a14ca7c690817b1e3cfad551759d7a34187a968b
root <root@STDLTUC-L069.localdomain>
2021-06-21 10:08:18 +0300

    fix

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './header.css';
-
+import LogoutButton from './LogoutButton';
 class Header extends React.Component {
   render() {
     return(
@@ -12,6 +12,7 @@ class Header extends React.Component {
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
           {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+          <LogoutButton />
       </Navbar>
     )
   }
