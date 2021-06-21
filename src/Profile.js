a737687528f47c5c293252e902960b9f75fcaa6c
root <root@STDLTUC-L069.localdomain>
2021-06-21 23:11:07 +0300

    lab 12

--- src/Profile.js
+++ src/Profile.js
@@ -1,28 +1,34 @@
 import { withAuth0 } from '@auth0/auth0-react';
-import React, { Component } from 'react'
-import Image from 'react-bootstrap/Image'
+import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
+import BestBook from './BestBook';
+
+
+
 export class Profile extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             userName: this.props.auth0.user.name,
             userEmail: this.props.auth0.user.email,
             userPic: this.props.auth0.user.picture,
-
-
+            userData: []
         }
     }
+
     render() {
         return (
             <div>
-                <h2>{this.state.userName}</h2>
-                <p>{this.state.userEmail}</p>
-                <Image src={this.state.userPic} rounded  alt ={this.state.userName}/>
-
-             
+                <div>
+                    <h2>{this.state.userName}</h2>
+                    <p>{this.state.userEmail}</p>
+                    <Image src={this.state.userPic} rounded alt={this.state.userName} />
+                </div>
+                <BestBook
+                userEmail={this.state.userEmail} />
             </div>
         )
     }
 }
 
-export default  withAuth0(Profile);
+export default withAuth0(Profile);
